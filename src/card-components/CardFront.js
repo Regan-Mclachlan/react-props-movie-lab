3a60858167052879ff149855d635dd214f030cff
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-31 21:31:32 +1000

    almost complete

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+// import movieData from './data.js'
 export default class CardFront extends Component {
 
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
     )
   }
